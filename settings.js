36772e21e80aa1438c8257bdb1082620c72dcc90
HANSTZ <hanstzmd@gmail.com>
2025-12-17 15:17:41 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Hans',
   botName: "Hans_Tz",
   botOwner: 'Hans_Tz', // Your name
-  ownerNumber: '255760774888', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   maxStoreMessages: 20, 
