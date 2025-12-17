4c4cb65a3bd9243d166d5bf11df01fc82287795e
HANSTZ <hanstzmd@gmail.com>
2025-12-17 12:01:53 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Hans_Tz',
+  author: 'Hans',
+  botName: "Hans_Tz",
+  botOwner: 'Hans_Tz', // Your name
+  ownerNumber: '255760774888', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "private",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.0",
+  updateZipUrl: "https://github.com/HansTz/VORTEX-XMD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
