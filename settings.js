a8c218bd148f0f2abf106a8861c7d39ddc67df3a
HANSTZ <hanstzmd@gmail.com>
2026-01-06 16:50:48 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,13 @@
 const settings = {
-  packname: 'HansTz',
+  packname: 'Hans_Tz',
   author: 'Hans',
-  botName: "HansTz",
-  botOwner: 'HansTz', // Your name
-  ownerNumber: '', //Set your number here without + symbol, just add country code & number without any space
+  SESSION_ID: '', //Your Session id Example: HansTz&abc123def456ghi789
+  botName: "Hans_Tz",
+  botOwner: 'Hans_Tz',
+  ownerNumber: '255760774888',
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
-  maxStoreMessages: 20, 
+  maxStoreMessages: 20,
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.0",
