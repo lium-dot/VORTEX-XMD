8fca5782120bc8db6a464dcb20f8021fd34206c0
HANSTZ <hanstzmd@gmail.com>
2026-01-07 23:09:26 +0300

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 const settings = {
-packname: process.env.PACK_NAME || 'Hans_Tz',
-  
-author: process.env.AUTHOR || 'Hans',
-  
+
 SESSION_ID: process.env.SESSION_ID || '',
   
 botName: process.env.BOT_NAME || "Hans_Tz",
@@ -13,14 +10,8 @@ Botprefix: process.env.PREFIX || '.',
   
 ownerNumber: process.env.OWNER_NUMBER || '255760774888',
   
-giphyApiKey: process.env.GIPHY_API_KEY || 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  
 commandMode: process.env.COMMAND_MODE || "private",
-  
-maxStoreMessages: parseInt(process.env.MAX_STORE_MESSAGES) || 20,
-  
-storeWriteInterval: parseInt(process.env.STORE_WRITE_INTERVAL) || 10000,
-  
+   
 description: process.env.BOT_DESCRIPTION || "This is a bot for managing group commands and automating tasks.",
   
 version: process.env.BOT_VERSION || "2.0.0",
